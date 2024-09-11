c7d7d171d78e7ddb1e1cbb8199e792133679d951
Mateusz Czernek <czernek.mateusz@gmail.com>
2024-09-11 22:03:27 +0200

    Implementation verified with big sample of test data.

--- Kata/Kata.Core/Anagrams/AnagramsKata.cs
+++ Kata/Kata.Core/Anagrams/AnagramsKata.cs
@@ -1,17 +1,24 @@
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
 using Kata.Common.Files;
 
 namespace Kata.Core.Anagrams;
 
+// Source of the Kata (along with sample file)
+// http://codekata.com/kata/kata06-anagrams/
+
 public static class AnagramsKata
 {
-    public static IEnumerable<string> FindAnagramsInFile(string pathToFileWithWords)
+    public static IReadOnlyDictionary<string, List<string>> FindWordsWithAnagramsInFile(string pathToFileWithWords)
     {
         if (string.IsNullOrEmpty(pathToFileWithWords))
             throw new ArgumentException("Value cannot be null or empty.", nameof(pathToFileWithWords));
-
-        var anagramsGroups = new Dictionary<string, List<string>>();
+        
+        var wordsWithAnagrams = new Dictionary<string, List<string>>();
         var wordsFromFile = FileReader.ReadNonEmptyLines(pathToFileWithWords);
 
+        var anagramsCandidates = new Dictionary<string, List<string>>();
+        
         foreach (var word in wordsFromFile)
         {
             var wordAsArrayOfChar = word.ToCharArray();
@@ -19,18 +26,18 @@ public static class AnagramsKata
 
             var anagramGroupKey = string.Concat(wordAsArrayOfChar);
 
-            if (anagramsGroups.TryGetValue(anagramGroupKey, out var anagramGroupList))
+            if (anagramsCandidates.TryGetValue(anagramGroupKey, out var anagramGroupList))
                 anagramGroupList.Add(word);
             else
-                anagramsGroups.Add(anagramGroupKey, [word]);
+                anagramsCandidates.Add(anagramGroupKey, [word]);
         }
         
-        foreach (var group in anagramsGroups)
+        foreach (var group in anagramsCandidates)
         {
             if (group.Value.Count <= 1) continue;
-            
-            foreach (var anagram in group.Value)
-                yield return anagram;
+            wordsWithAnagrams.Add(group.Key, group.Value);
         }
+
+        return wordsWithAnagrams;
     }
 }
--- Kata/Kata.Tests/Anagrams/AnagramsTests.cs
+++ Kata/Kata.Tests/Anagrams/AnagramsTests.cs
@@ -10,9 +10,23 @@ public class AnagramsTests
     public void Anagrams_Find_SmallSample()
     {
         var filePathWithSmallSample = @"TestData\Anagrams\small_sample.txt".ToProjectDirectory();
-        var anagramsFromFile = AnagramsKata.FindAnagramsInFile(filePathWithSmallSample).ToArray();
+        var wordsWithAnagrams = AnagramsKata.FindWordsWithAnagramsInFile(filePathWithSmallSample).ToArray();
+        Assert.That(wordsWithAnagrams.Count, Is.EqualTo(3));
         
-        Assert.That(anagramsFromFile.Count, Is.EqualTo(8));
-        Assert.That(anagramsFromFile, Is.EquivalentTo(new[] { "maters", "stream", "gallery", "largely", "regally", "art", "rat", "tar" }));
+        var anagramsOnly = wordsWithAnagrams.SelectMany(x => x.Value);
+        Assert.That(anagramsOnly,
+            Is.EquivalentTo(new[] { "maters", "stream", "gallery", "largely", "regally", "art", "rat", "tar" }));
+    }
+
+    [Test]
+    public void Anagrams_Find_BigSample()
+    {
+        // Source of the Kata (along with sample file)
+        // http://codekata.com/kata/kata06-anagrams/
+
+        var filePathWithBigSample = @"TestData\Anagrams\wordlist_codingdojo_expected_20683_anagrams.txt".ToProjectDirectory();
+        var wordsWithAnagrams = AnagramsKata.FindWordsWithAnagramsInFile(filePathWithBigSample).ToArray();
+
+        Assert.That(wordsWithAnagrams.Count, Is.EqualTo(20683));
     }
 }
